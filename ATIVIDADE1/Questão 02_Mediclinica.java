918ddd78d3099298077c7b408fcc786f474bb767
Magnum2222 <magnumsapf@gmail.com>
2021-10-15 21:29:52 -0300

    Create Questão 02_Mediclinica.java

---
+++ ATIVIDADE1/Questão 02_Mediclinica.java
@@ -0,0 +1,14 @@
+package ATIVIDADE1;
+public class Questao02_Mediclinica {
+
+    int Codigo; String Nome, Sexo, Especialidade; Questao02_Mediclinica end;
+
+    public Questao02_Mediclinica(int Codigo, String Nome, String Sexo, String Especiallidade, Questao02_Mediclinica end){
+
+        this.Codigo = Codigo;
+        this.Nome = Nome;
+        this.Sexo = Sexo;
+        this.Especialidade = Especialidade;
+        this.end = end;
+    }
+}
